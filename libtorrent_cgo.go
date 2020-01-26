86f25ca9cb73d73ffa1d42c0ee78737f60d7952e
i96751414 <i96751414@gmail.com>
2020-01-26 23:53:05 +0000

    Make sure we static link on windows

--- libtorrent_cgo.go
+++ libtorrent_cgo.go
@@ -8,5 +8,5 @@ package libtorrent
 // #cgo linux CXXFLAGS: -std=c++11 -I/usr/include/libtorrent -Wno-deprecated-declarations
 // #cgo linux LDFLAGS: -lm -lstdc++ -ldl -lrt
 // #cgo windows CXXFLAGS: -std=c++11 -DIPV6_TCLASS=39 -D_WIN32_WINNT=0x0600 -D__MINGW32__
-// #cgo windows LDFLAGS: -static-libgcc -static-libstdc++
+// #cgo windows LDFLAGS: -static-libgcc -static-libstdc++ -static
 import "C"
