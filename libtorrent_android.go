f691df2bfe7feb13ea756261da81453dd57c837c
i96751414 <i96751414@gmail.com>
2020-01-29 23:38:20 +0000

    Use only one cgo file

--- libtorrent_android.go
+++
@@ -1,7 +0,0 @@
-// +build android
-
-package libtorrent
-
-// #cgo pkg-config: --static libtorrent-rasterbar openssl
-// #cgo android LDFLAGS: -lm -lc++_shared -ldl
-import "C"
--- libtorrent_cgo.go
+++ libtorrent_cgo.go
@@ -1,12 +1,11 @@
-// +build !android
-
 package libtorrent
 
 // #cgo pkg-config: --static libtorrent-rasterbar openssl
 // #cgo darwin CXXFLAGS: -std=c++11 -fvisibility=hidden -fvisibility-inlines-hidden
 // #cgo darwin LDFLAGS: -lm -lstdc++ -framework CoreFoundation -framework SystemConfiguration
-// #cgo linux CXXFLAGS: -std=c++11 -I/usr/include/libtorrent -Wno-deprecated-declarations
-// #cgo linux LDFLAGS: -lm -lstdc++ -ldl -lrt
+// #cgo !android,linux CXXFLAGS: -std=c++11 -I/usr/include/libtorrent -Wno-deprecated-declarations
+// #cgo !android,linux LDFLAGS: -lm -lstdc++ -ldl -lrt
+// #cgo android LDFLAGS: -lm -lc++_shared -ldl
 // #cgo windows CXXFLAGS: -std=c++11 -DIPV6_TCLASS=39 -D_WIN32_WINNT=0x0600 -D__MINGW32__
 // #cgo windows LDFLAGS: -static-libgcc -static-libstdc++ -static
 import "C"
