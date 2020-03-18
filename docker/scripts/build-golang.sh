acbb4d03f1defaa7225fb27f6a3d102ddfe083cb
i96751414 <i96751414@gmail.com>
2020-03-18 22:34:20 +0000

    Update go to version 1.14

--- docker/scripts/build-golang.sh
+++ docker/scripts/build-golang.sh
@@ -3,7 +3,7 @@
 set -ex
 mkdir -p /usr/local/bootstrap
 if [ ! -f "golang-bootstrap.tar.gz" ]; then
-  wget -q "$GOLANG_BOOTSTRAP_URL" -O golang-bootstrap.tar.gz
+  wget -q "https://dl.google.com/go/go{$GOLANG_BOOTSTRAP_VERSION}.tar.gz" -O golang-bootstrap.tar.gz
 fi
 echo "$GOLANG_BOOTSTRAP_SHA256  golang-bootstrap.tar.gz" | sha256sum -c -
 tar -C /usr/local/bootstrap -xzf golang-bootstrap.tar.gz
@@ -14,7 +14,7 @@ export GOROOT_BOOTSTRAP=/usr/local/bootstrap/go
 
 cd /build
 if [ ! -f "golang.tar.gz" ]; then
-  wget -q "$GOLANG_SRC_URL" -O golang.tar.gz
+  wget -q "https://golang.org/dl/go${GOLANG_VERSION}.src.tar.gz" -O golang.tar.gz
 fi
 echo "$GOLANG_SRC_SHA256  golang.tar.gz" | sha256sum -c -
 tar -C /usr/local -xzf golang.tar.gz
